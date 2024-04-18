465125ccea714d96c5d1fb7bbab253082a9ef74e
Dylan Robinaud <dylan.rbnd@gmail.com>
2024-04-18 08:36:32 +0200

    Rajout message validation formulaire + modification formulaire

--- contact.js
+++ contact.js
@@ -4,6 +4,14 @@ contact.addEventListener("submit", (event) => {
   emailjs.sendForm("service_zjs9223", "template_ygcizvt", event.target).then(
     () => {
       console.log("SUCCESS!");
+      const successMessage = document.createElement("p");
+      successMessage.textContent = "Le formulaire a été envoyé avec succès!";
+      successMessage.classList.add("success-message");
+      contact.appendChild(successMessage);
+      setTimeout(() => {
+        successMessage.remove();
+        contact.reset();
+      }, 2000);
     },
     (error) => {
       console.log("FAILED...", error);
